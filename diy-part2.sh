e3e2f8bf5d52ff24183353846555f4aed6c86413
lmxslms <41403627+lmxslms@users.noreply.github.com>
2020-10-29 14:49:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,15 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#修改版本号
+sed -i 's/OpenWrt/lmxslmxs Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
+
+# Add luci-app-usb-printer
+git clone https://github.com/lmxslmslmxslms/OpenWrt-packages-1/tree/master/luci-app-usb-printer
+
+# Add luci-app-ttyd
+https://github.com/lmxslmslmxslms/OpenWrt-packages-1/tree/master/luci-app-ttyd
+
+# Add luci-theme-infinityfreedom
+svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom package/openwrt-packages/luci-theme-infinityfreedom
